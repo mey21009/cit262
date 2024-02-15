b959d9248ae78dbba2890d59beefdd0a94bd8084
mey21009 <mey2100@byui.edu>
2024-02-14 19:52:00 -0700

    manual?

--- utils/helloworld.mjs
+++ utils/helloworld.mjs
@@ -1,5 +1,7 @@
-const sayHello=()=>{
-    return "hello"
-
+const sayHello = () => {
+    const message = "hello";
+    console.log(message); // Adding console.log() to print "hello"
+    return message;
 }
+
 export default sayHello;
